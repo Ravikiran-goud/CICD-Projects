26664b4004f76460a81181f23a15ffcd4ea68cf8
Ravi Kiran Goud Jaggari <122633050+Ravikiran-goud@users.noreply.github.com>
2025-01-17 19:57:55 -0500

    python file

--- github_actions/src/app.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(a, b):
-    """Add two numbers and return the result."""
-    return a + b
-
-if __name__ == "__main__":
-    x = 5
-    y = 10
-    print(f"The sum of {x} and {y} is {add_numbers(x, y)}")
